77298364f1c046e6d29db4cd6e57be3d4851feab
VictorBarrio <barriosarrasecavictor@gmail.com>
2023-01-15 17:26:20 +0100

    Primer commit

---
+++ VBPrimerProyecto/src/hola.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class hola {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+        int valor;
+        System.out.println("Escribe el valor");
+        valor=teclado.nextInt();
+        
+        for(int fila=1;fila<=valor;fila++){
+            for (int columna = 1; columna <= valor; columna++){
+                    System.out.print("* ");	
+            }
+                    System.out.println();
+        }
+	}
+}
